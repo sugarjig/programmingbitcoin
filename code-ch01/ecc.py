ab08e79c83d0d4991f8c3ef8e66dfeabc875a58d
Sam Jones <sam.jones@willowtreeapps.com>
2021-06-02 15:57:34 -0400

    Chapter 1 Exercise 1

--- code-ch01/ecc.py
+++ code-ch01/ecc.py
@@ -23,7 +23,7 @@ class FieldElement:
 
     def __ne__(self, other):
         # this should be the inverse of the == operator
-        raise NotImplementedError
+        return not (self == other)
 
     # tag::source2[]
     def __add__(self, other):
