966e41fe09de51a28f52bcf69fe1aa042b2f343b
Sam Jones <git@samjones.io>
2021-06-30 20:44:58 -0400

    Chapter 5 Exercise 2

--- code-ch05/tx.py
+++ code-ch05/tx.py
@@ -110,14 +110,18 @@ class Tx:
         '''
         # s.read(n) will return n bytes
         # version is an integer in 4 bytes, little-endian
-        version = int.from_bytes(s.read(4), 'little')
+        version = little_endian_to_int(s.read(4))
         # num_inputs is a varint, use read_varint(s)
+        num_inputs = read_varint(s)
         # parse num_inputs number of TxIns
+        tx_ins = []
+        for i in range(num_inputs):
+            tx_ins.append(TxIn.parse(s))
         # num_outputs is a varint, use read_varint(s)
         # parse num_outputs number of TxOuts
         # locktime is an integer in 4 bytes, little-endian
         # return an instance of the class (see __init__ for args)
-        return Tx(version, None, None, None, testnet)
+        return cls(version, tx_ins, None, None, testnet)
 
     # tag::source6[]
     def serialize(self):
@@ -166,11 +170,15 @@ class TxIn:
         return a TxIn object
         '''
         # prev_tx is 32 bytes, little endian
+        prev_tx = s.read(32)[::-1]
         # prev_index is an integer in 4 bytes, little endian
+        prev_index = little_endian_to_int(s.read(4))
         # use Script.parse to get the ScriptSig
+        script_sig = Script.parse(s)
         # sequence is an integer in 4 bytes, little-endian
+        sequence = little_endian_to_int(s.read(4))
         # return an instance of the class (see __init__ for args)
-        raise NotImplementedError
+        return cls(prev_tx, prev_index, script_sig, sequence)
 
     # tag::source5[]
     def serialize(self):
