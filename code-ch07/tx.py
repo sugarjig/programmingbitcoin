03475d3ed9c4666e1d913f5f21f4a6e9b48dce5b
Sam Jones <git@samjones.io>
2021-07-05 21:33:05 -0400

    Chapter 7 Exercise 1

--- code-ch07/tx.py
+++ code-ch07/tx.py
@@ -162,19 +162,32 @@ class Tx:
         signed for index input_index'''
         # start the serialization with version
         # use int_to_little_endian in 4 bytes
+        result = int_to_little_endian(self.version, 4)
         # add how many inputs there are using encode_varint
+        result += encode_varint(len(self.tx_ins))
         # loop through each input using enumerate, so we have the input index
+        for i, tx_in in enumerate(self.tx_ins):
             # if the input index is the one we're signing
             # the previous tx's ScriptPubkey is the ScriptSig
+            if i == input_index:
+                result += TxIn(tx_in.prev_tx, tx_in.prev_index, tx_in.script_pubkey(self.testnet), tx_in.sequence).serialize()
             # Otherwise, the ScriptSig is empty
+            else:
+                result += TxIn(tx_in.prev_tx, tx_in.prev_index, None, tx_in.sequence).serialize()
             # add the serialization of the input with the ScriptSig we want
         # add how many outputs there are using encode_varint
+        result += encode_varint(len(self.tx_outs))
         # add the serialization of each output
+        for tx_out in self.tx_outs:
+            result += tx_out.serialize()
         # add the locktime using int_to_little_endian in 4 bytes
+        result += int_to_little_endian(self.locktime, 4)
         # add SIGHASH_ALL using int_to_little_endian in 4 bytes
+        result += int_to_little_endian(0x01, 4)
         # hash256 the serialization
+        h256 = hash256(result)
         # convert the result to an integer using int.from_bytes(x, 'big')
-        raise NotImplementedError
+        return int.from_bytes(h256, 'big')
 
     def verify_input(self, input_index):
         '''Returns whether the input has a valid signature'''
