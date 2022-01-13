c0274a6c73eda54df93070aa393aee62e501c0fc
Sam Jones <github@samjones.io>
2022-01-12 20:34:03 -0500

    Chapter 9 Exercise 1

--- code-ch09/tx.py
+++ code-ch09/tx.py
@@ -258,10 +258,17 @@ class Tx:
     def is_coinbase(self):
         '''Returns whether this transaction is a coinbase transaction or not'''
         # check that there is exactly 1 input
+        if len(self.tx_ins) != 1:
+            return False
         # grab the first input
+        tx_in = self.tx_ins[0]
         # check that first input prev_tx is b'\x00' * 32 bytes
+        if tx_in.prev_tx != b'\x00' * 32:
+            return False
         # check that first input prev_index is 0xffffffff
-        raise NotImplementedError
+        if tx_in.prev_index != 0xffffffff:
+            return False
+        return True
 
     def coinbase_height(self):
         '''Returns the height of the block this coinbase transaction is in
