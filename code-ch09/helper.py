969b6aaec68a7ba0a82dbf849a03408938a07ce1
Sam Jones <github@samjones.io>
2022-01-17 17:37:02 -0500

    Chapter 9 Exercise 9

--- code-ch09/helper.py
+++ code-ch09/helper.py
@@ -126,10 +126,12 @@ def h160_to_p2sh_address(h160, testnet=False):
 def bits_to_target(bits):
     '''Turns bits into a target (large 256-bit integer)'''
     # last byte is exponent
+    exponent = bits[-1]
     # the first three bytes are the coefficient in little endian
+    coefficient = little_endian_to_int(bits[:-1])
     # the formula is:
     # coefficient * 256**(exponent-3)
-    raise NotImplementedError
+    return (coefficient * 256**(exponent-3))
 
 
 # tag::source1[]
