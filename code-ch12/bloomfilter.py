36fdc2b8f5a9bdf5e60c8f4d6163cf625332a4ae
Sam Jones <github@samjones.io>
2022-01-27 19:21:42 -0500

    Chapter 12 Exercise 3

--- code-ch12/bloomfilter.py
+++ code-ch12/bloomfilter.py
@@ -25,11 +25,15 @@ class BloomFilter:
     def add(self, item):
         '''Add an item to the filter'''
         # iterate self.function_count number of times
+        for i in range(self.function_count):
             # BIP0037 spec seed is i*BIP37_CONSTANT + self.tweak
+            seed = i * BIP37_CONSTANT + self.tweak
             # get the murmur3 hash given that seed
+            h = murmur3(item, seed=seed)
             # set the bit at the hash mod the bitfield size (self.size*8)
+            bit = h % (self.size * 8)
             # set the bit field at bit to be 1
-        raise NotImplementedError
+            self.bit_field[bit] = 1
 
     def filter_bytes(self):
         return bit_field_to_bytes(self.bit_field)
